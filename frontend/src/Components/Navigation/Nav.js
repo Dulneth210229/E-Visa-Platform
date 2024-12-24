42d2b8fd9ee2a99588376c884093801091f6270f
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-24 20:49:39 +0530

    create navigation items

--- frontend/src/Components/Navigation/Nav.js
+++ frontend/src/Components/Navigation/Nav.js
@@ -12,7 +12,23 @@ function Nav() {
           <a href="/">
             <i className="fas fa-home"></i> Home
           </a>
-          <a href="/">Apply Now</a>
+          <a href="/">
+            <i className="fas fa-paper-plane"></i>Apply Now
+          </a>
+          <a href="/">
+            <i className="fas fa-info-circle"></i>Visa Status
+          </a>
+          <a href="/">
+            <i className="fas fa-question-circle"></i>Help
+          </a>
+        </div>
+        <div class="nav-login">
+          <a href="/">
+            <i className="fas fa-sign-in-alt"></i>Login
+          </a>
+          <a href="/">
+            <i className="fas fa-user-plus"></i>Signup
+          </a>
         </div>
       </nav>
     </div>
