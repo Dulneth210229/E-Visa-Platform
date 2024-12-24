3e4419c919dace6eceef3387fad0a5f1ea67cb36
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-24 22:54:21 +0530

    create navigation bar

--- frontend/src/Components/Navigation/Nav.js
+++ frontend/src/Components/Navigation/Nav.js
@@ -1,34 +1,39 @@
 import React from "react";
+import "./Nav.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import logo from "./images/Logo.png";
 
 function Nav() {
   return (
     <div>
       {/*Navigation bar */}
       <nav class="navbar">
-        <div class="nav-content"></div>
-        <div class="logo"></div>
-        <div class="nav-links">
-          <a href="/">
-            <i className="fas fa-home"></i> Home
-          </a>
-          <a href="/">
-            <i className="fas fa-paper-plane"></i>Apply Now
-          </a>
-          <a href="/">
-            <i className="fas fa-info-circle"></i>Visa Status
-          </a>
-          <a href="/">
-            <i className="fas fa-question-circle"></i>Help
-          </a>
-        </div>
-        <div class="nav-login">
-          <a href="/">
-            <i className="fas fa-sign-in-alt"></i>Login
-          </a>
-          <a href="/">
-            <i className="fas fa-user-plus"></i>Signup
-          </a>
+        <div class="nav-content">
+          <div class="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div class="nav-links">
+            <a href="/">
+              <i className="fas fa-home"></i> <span>Home</span>
+            </a>
+            <a href="/">
+              <i className="fas fa-paper-plane"></i>Apply Now
+            </a>
+            <a href="/">
+              <i className="fas fa-info-circle"></i>Visa Status
+            </a>
+            <a href="/">
+              <i className="fas fa-question-circle"></i>Help
+            </a>
+          </div>
+          <div class="nav-login">
+            <a href="/">
+              <i className="fas fa-sign-in-alt"></i>Login
+            </a>
+            <a href="/">
+              <i className="fas fa-user-plus"></i>Signup
+            </a>
+          </div>
         </div>
       </nav>
     </div>
