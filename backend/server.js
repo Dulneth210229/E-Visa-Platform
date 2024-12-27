c3eb9a6cce01d151de3eb8a40b5d7a1b0b957d91
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-28 00:15:41 +0530

    some changes in the server.js file

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files (uploaded images, PDFs, etc.)
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("./uploads", express.static(path.join(__dirname, "uploads")));
 
 //Route handling
 app.use("/api/visa", visaRouter);
