722d4e93ce0543975751db0bf8a4e57ebc25ebd6
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-27 14:56:13 +0530

    complete the backend controller class with all the CRUD operations

--- backend/Controllers/VisaController.js.js
+++ backend/Controllers/VisaController.js.js
@@ -37,4 +37,125 @@ const addVisa = async (req, res, next) => {
     birthCertificate,
     policeCertificate,
   } = req.body;
+
+  let visa;
+  try {
+    visa = new Visa({
+      firstName,
+      lastName,
+      DOB,
+      email,
+      phoneNumber,
+      country,
+      address,
+      postalCode,
+      visaType,
+      visaDuration,
+      purpose,
+      passportCopy,
+      birthCertificate,
+      policeCertificate,
+    });
+    await visa.save();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!visa) {
+    return res.status(404).json({ message: "Visa not found" });
+  }
+  // Display all visas
+  return res.status(200).jason({ visa });
 };
+
+//Get visa by Id
+const getVisaById = async (req, res, next) => {
+  const visaId = req.params.id;
+  let visa;
+  try {
+    visa = await Visa.findById(visaId);
+  } catch (err) {
+    console.log(err);
+  }
+  //not available visa
+  if (!visa) {
+    return res.status(404).json({ message: "Visa not found" });
+  }
+  //display visa by id
+  return res.status(200).json({ visa });
+};
+
+//update visa details
+const updateVisa = async (req, res, next) => {
+  const visaId = req.params.id;
+  const {
+    firstName,
+    lastName,
+    DOB,
+    email,
+    phoneNumber,
+    country,
+    address,
+    postalCode,
+    visaType,
+    visaDuration,
+    purpose,
+    passportCopy,
+    birthCertificate,
+    policeCertificate,
+  } = req.body;
+
+  let visa;
+  try {
+    visa = await Visa.findByIdAndUpdate(visaId, {
+      firstName,
+      lastName,
+      DOB,
+      email,
+      phoneNumber,
+      country,
+      address,
+      postalCode,
+      visaType,
+      visaDuration,
+      purpose,
+      passportCopy,
+      birthCertificate,
+      policeCertificate,
+    });
+    visa = await visa.save();
+  } catch (err) {
+    console.log(err);
+  }
+  //not available visa
+  if (!visa) {
+    return res.status(404).json({ message: "Unable to update visa" });
+  }
+  //display visa by id
+  return res.status(200).json({ visa });
+};
+
+//Delete visa
+const deleteVisa = async (req, res, next) => {
+  const visaId = req.params.id;
+
+  let visa;
+  try {
+    visa = await Visa.findByIdAndDelete(visaId);
+  } catch (err) {
+    console.log(err);
+  }
+  //not available visa
+  if (!visa) {
+    return res.status(404).json({ message: "Unable to delete visa" });
+  }
+  //display visa by id
+  return res.status(200).json({ message: "Visa deleted successfully" });
+};
+
+//Export to routs
+exports.getVisa = getVisa;
+exports.addVisa = addVisa;
+exports.getVisaById = getVisaById;
+exports.updateVisa = updateVisa;
+exports.deleteVisa = deleteVisa;
