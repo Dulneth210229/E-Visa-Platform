34fb0d2317cda462a5468245f1a87822bf6de2d1
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-09 21:53:36 +0530

    hasing and encrypting password and some changes on controller fille as well

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -7,7 +7,17 @@ const loginUser = async (req, res) => {
 };
 
 //Signup user
-const signupUser = async (req, res) => {
+const signupUser = async function (req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+
   res.json({ message: "Signup user" });
 };
 
--- backend/Model/UserModel.js
+++ backend/Model/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Scheme = mongoose.Schema;
 
 const UserScheme = new Scheme({
@@ -12,5 +13,21 @@ const UserScheme = new Scheme({
     required: true,
   },
 });
+
+//static method to signup user
+UserScheme.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw error("Email already inuse");
+  }
+
+  //generate the salt
+  const salt = await bcrypt.genSalt(10);
+  //hash the password for extra security
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model("User", UserScheme);
