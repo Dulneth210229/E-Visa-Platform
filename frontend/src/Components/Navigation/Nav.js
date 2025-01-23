06c3e56dc75b88c80bc924d7cc2087ce4a43f2fd
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-23 21:09:07 +0530

    Logout function and get user email
    Get user detail from the local storage

--- frontend/src/Components/Navigation/Nav.js
+++ frontend/src/Components/Navigation/Nav.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./Nav.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import logo from "./images/Logo.png";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Nav() {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <div>
       {/*Navigation bar */}
@@ -27,12 +31,25 @@ function Nav() {
             </a>
           </div>
           <div class="nav-login">
-            <a href="/login">
-              <i className="fas fa-sign-in-alt"></i>Login
-            </a>
-            <a href="/signup">
-              <i className="fas fa-user-plus"></i>Signup
-            </a>
+            {user && (
+              <>
+                <span class="user-mail">{user.email}</span>
+                <a href="/login" onClick={logout}>
+                  <i class="fa-solid fa-right-from-bracket"></i>
+                  Logout
+                </a>
+              </>
+            )}
+            {!user && (
+              <>
+                <a href="/login">
+                  <i className="fas fa-sign-in-alt"></i>Login
+                </a>
+                <a href="/signup">
+                  <i className="fas fa-user-plus"></i>Signup
+                </a>
+              </>
+            )}
           </div>
         </div>
       </nav>
